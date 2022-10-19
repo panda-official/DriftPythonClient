5f33a2f5dfb10ab011e4afd2e3ece3e231cea89c
ciaran moyne <moyne@panda.technology>
2022-10-19 16:12:01 +0200

    fix linter error for unused argument

--- tests/minio_client_test.py
+++ tests/minio_client_test.py
@@ -6,7 +6,7 @@ from drift_client.minio_client import MinIOClient
 from drift_client.error import DriftClientError
 
 
-@pytest.fixture(name="minio_client")
+@pytest.fixture(name="_minio_client")
 def _make_minio_client(mocker):
     """fake client which just raises an S3error"""
     client_klass = mocker.patch("drift_client.minio_client.Minio")
@@ -26,8 +26,9 @@ def _make_minio_client(mocker):
     return client_klass
 
 
-def test__rasise_drift_error(minio_client):
+def test__rasise_drift_error(_minio_client):
     """make sure client raises correct error"""
+
     test_path = "non_existant_data.ext"
     client = MinIOClient("localhost:9000", "user", "password", secure=False)
     with pytest.raises(DriftClientError, match=f"Could not read item at {test_path}"):
