7b27070b7746431b929d891fc09b8558ffa8bbf8
Alexey Timin <atimin@gmail.com>
2023-06-16 16:47:57 +0200

    debug

--- pkg/drift_client/__init__.py
+++ pkg/drift_client/__init__.py
@@ -3,15 +3,3 @@
 
 from drift_client.drift_client import DriftClient
 from drift_client.drift_data_package import DriftDataPackage
-
-
-def _load_version() -> str:
-    """Load version from VERSION file"""
-    from pathlib import Path  # pylint: disable=import-outside-toplevel
-
-    here = Path(__file__).parent.resolve()
-    with open(here / "VERSION", "r", encoding="utf-8") as version_file:
-        return version_file.read().strip()
-
-
-__version__ = _load_version()
