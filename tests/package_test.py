e801ba51a2c88edfd61930c86fd1760b63d43d86
Alexey Timin <atimin@gmail.com>
2023-09-14 11:39:16 +0200

    Add support for Typed Data (#35)
    * add support for typed data
    
    * update CHANGELOG
    
    * fix pylint
    
    * add docsting

--- tests/package_test.py
+++ tests/package_test.py
@@ -1,16 +1,20 @@
 """Tests for Package"""
+from typing import Dict
+
 # pylint: disable=no-member
 import numpy as np
 import pytest
+from google.protobuf.any_pb2 import Any  # pylint: disable=no-name-in-module)
+
+from drift_bytes import Variant, OutputBuffer
+from drift_protocol.meta import TypedDataInfo, MetaInfo
+from drift_protocol.common import DriftPackage, StatusCode, DataPayload
+from drift_client import DriftDataPackage
 from wavelet_buffer import (  # pylint: disable=no-name-in-module
     WaveletBuffer,
     WaveletType,
     denoise,
 )
-from drift_protocol.common import DriftPackage, StatusCode, DataPayload
-from google.protobuf.any_pb2 import Any  # pylint: disable=no-name-in-module)
-
-from drift_client import DriftDataPackage
 
 
 @pytest.fixture(name="signal")
@@ -39,8 +43,6 @@ def _make_good_package(buffer: WaveletBuffer) -> DriftPackage:
     pkg.publish_timestamp.FromMilliseconds(2000)
 
     payload = DataPayload()
-    payload.shape.append(1)
-    payload.shape.append(buffer.parameters.signal_shape[0])
     payload.data = buffer.serialize()
 
     any_msg = Any()
@@ -53,6 +55,48 @@ def _make_good_package(buffer: WaveletBuffer) -> DriftPackage:
     label.value = "value"
 
     pkg.labels.append(label)
+
+    return pkg
+
+
+@pytest.fixture(name="typed_data")
+def _make_typed_data() -> Dict[str, Variant.SUPPORTED_TYPES]:
+    return {"bool": True, "int": 1, "float": 1.0, "string": "string", "none": None}
+
+
+@pytest.fixture(name="typed_data_package")
+def _make_typed_data_package(
+    typed_data: Dict[str, Variant.SUPPORTED_TYPES]
+) -> DriftPackage:
+    pkg = DriftPackage()
+    pkg.id = 1
+    pkg.status = StatusCode.GOOD
+    pkg.source_timestamp.FromMilliseconds(1000)
+    pkg.publish_timestamp.FromMilliseconds(2000)
+
+    buffer = OutputBuffer()
+    items = TypedDataInfo()
+    for key, value in typed_data.items():
+        item = TypedDataInfo.Item()
+        item.name = key
+        if value is None:
+            item.status = StatusCode.BAD
+            buffer.push(Variant(False))
+        else:
+            item.status = StatusCode.GOOD
+            buffer.push(Variant(value))
+        items.items.append(item)
+
+    pkg.meta.type = MetaInfo.TYPED_DATA
+    pkg.meta.typed_data_info.CopyFrom(items)
+
+    payload = DataPayload()
+    payload.data = buffer.bytes()
+
+    any_msg = Any()
+    any_msg.Pack(payload)
+
+    pkg.data.append(any_msg)
 
     return pkg
 
@@ -84,3 +128,9 @@ def test__labels(good_package):
     """Should provide access to labels"""
     pkg = DriftDataPackage(good_package.SerializeToString())
     assert pkg.labels == {"key": "value"}
+
+
+def test__typed_data(typed_data_package, typed_data):
+    """Should provide access to typed data"""
+    pkg = DriftDataPackage(typed_data_package.SerializeToString())
+    assert pkg.as_typed_data() == typed_data
